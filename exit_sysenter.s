1be5dd3291af4594749840aef0d90a456adc8906
Derrick W. Turk <dwt@terminusdatascience.com>
2016-01-27 08:09:27 -0600

    Add a write system call with sysenter.

--- exit_sysenter.s
+++ exit_sysenter.s
@@ -1,6 +1,27 @@
 # do an "exit" syscall, using sysenter instruction
+.section .data
+hello: .ascii "goodbye world\n"
+.set hellosize, . - hello
+
+.section .text
 .globl _start
 _start:
+# syscall 4 / write
+movl $4, %eax
+# stdout
+movl $1, %ebx
+# our string and its size
+movl $hello, %ecx
+movl $hellosize, %edx
+# for sysenter: push return address
+pushl $return
+pushl %ecx # save ecx, edx, ebp
+pushl %edx
+pushl %ebp
+movl %esp, %ebp # save current SP as base
+sysenter
+return:
+
 # syscall 1 / exit : status 13
 movl $1, %eax
 movl $13, %ebx
