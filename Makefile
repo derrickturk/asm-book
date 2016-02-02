52bf39ce9c820ec4d5d4133414663270c9594c9c
Derrick W. Turk <dwt@terminusdatascience.com>
2016-02-01 23:28:21 -0600

    Playing around with ld flags for handling PIC libraries.

--- Makefile
+++ Makefile
@@ -2,12 +2,17 @@ ALL=$(patsubst %.s,%,$(wildcard *.s))
 
 all: $(ALL)
 
+
 # this machine uses PIC libraries, so we have to let gcc do its magic here
 #   if we're going to call into libc
 # without -nostdlib, the CRT's _start will collide with ours, and we'll
 #   get confused looking for a main that doesn't exist 
 power: power.o
 	cc -nostdlib -m32 -lc -o power power.o
+
+# ... or
+power_plt: power_plt.o
+	ld -m elf_i386 -lc -dynamic-linker /lib/ld-linux.so.2 -o $@ $<
 
 %.o: %.s
 	as --32 $< -o $@
---
+++ power_plt.s
@@ -0,0 +1,91 @@
+# power function, following platform ABI
+#   (use GNU as GOT/PLT syntax)
+#   (although even without the @plt, the current ld invocation
+#     seems to fix these up)
+.section .data
+.Lformatstr:
+.asciz "%d ^ %d = %d\n"
+
+.section .text
+# power(n, m) -> n ^ m (m >= 0)
+.globl power
+.type power, @function
+power:
+pushl %ebp
+movl %esp, %ebp
+# store esi (ABI requires it to be preserved)
+#   can't use edx as it will get clobbered during imul later
+pushl %esi
+# load n
+movl 0x8(%ebp), %esi
+# load m
+movl 0xc(%ebp), %ecx
+# initialize result
+movl $1, %eax
+.Lloop:
+test %ecx, %ecx
+jz .Ldone
+decl %ecx
+# eax:edx = eax * esi
+imull %esi
+jmp .Lloop
+.Ldone:
+popl %esi
+# = movl %ebp, %esp; popl %ebp
+leave
+ret
+
+.globl _start
+_start:
+
+# calculate 2 ^ 7
+pushl $7
+pushl $2
+call power
+# pop arguments off stack
+addl $0x8, %esp
+# call printf
+pushl %eax
+pushl $7
+pushl $2
+pushl $.Lformatstr
+call printf@plt
+addl $0x10, %esp
+
+# calculate (-4 ^ 3)
+pushl $3
+pushl $-4
+call power
+# pop arguments off stack
+addl $0x8, %esp
+# call printf
+pushl %eax
+pushl $3
+pushl $-4
+pushl $.Lformatstr
+call printf@plt
+addl $0x10, %esp
+
+# calculate 3 ^ 0
+pushl $0
+pushl $3
+call power
+# pop arguments off stack
+addl $0x8, %esp
+# call printf
+pushl %eax
+pushl $0
+pushl $3
+pushl $.Lformatstr
+call printf@plt
+addl $0x10, %esp
+
+# exit
+movl $1, %eax
+xor %ebx, %ebx
+pushl $0 # bogus return address
+pushl %ecx
+pushl %edx
+pushl %ebp
+movl %esp, %ebp
+sysenter
